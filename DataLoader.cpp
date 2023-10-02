1523f2b4f8e1e451933bdc03a387d98bdba6aaf5
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2023-10-02 12:02:19 +0800

    completed data loading

--- DataLoader.cpp
+++ DataLoader.cpp
@@ -14,17 +14,36 @@ DataLoader::DataLoader(string file_name){
 void DataLoader::load_data(){
     ifstream file(this->file_name);
     string line;
-    while(getline(file, line)){
-        Record record;
+    while(getline(file, line, '\n')){
+        vector <string> record;
         stringstream ss(line);
         string attribute;
         while (getline(ss, attribute, '\t')){
-            
+            record.push_back(attribute);
         }
-        
+        try {
+            Record new_record = {
+                record[0],
+                static_cast<unsigned int>(stoi(record[1])),
+                static_cast<unsigned short int>(stoi(record[2])),
+                stof(record[3]),
+                stof(record[4]),
+                stof(record[5]),
+                static_cast<unsigned short int>(stoi(record[6])),
+                static_cast<unsigned short int>(stoi(record[7])),
+                static_cast<bool>(stoi(record[8]))
+            };
+            this->records.push_back(new_record);
+        } catch (const std::exception& e) {
+            cout << "Ignored row" << endl;
+            continue;  // Skip records with conversion errors
+        }
     }
 }
 
+
 int main(){
-    DataLoader DataLoader("games.txt");
+    DataLoader loader = DataLoader("games.txt");
+    cout << "Data loaded successfully!" << endl;
+    return 0;
 }
